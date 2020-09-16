055b823656875365910c6219a55e8b0aab41ab50
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-09-16 15:47:30 -0300

    fs2-data-xml
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

---
+++ modules/fs2-data-xml/src/main/scala/fs2/data/xml/errors.scala
@@ -0,0 +1,43 @@
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+/*
+ * Copyright 2019 Lucas Satabin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fs2
+package data
+package xml
+
+/** All errors encountered in processing an Xml documents
+  *  have a kind.
+  *  These errors are unique code defined in the various normative documents
+  *  and recommendations.
+  */
+abstract class XmlError(val name: String)
+
+/** Represents a syntax error according to an XML production rule. */
+case class XmlSyntax(id: String) extends XmlError(f"XML [$id]")
+
+/** Represents a namespace constraint as defined in the XML namespaces recommendation. */
+sealed abstract class NSError(name: String) extends XmlError(name)
+case object NSCPrefixDeclared extends NSError("[NSC: Prefix Declared]")
+case object NSCNoPrefixUndeclaring extends NSError("[NSC: No Prefix Undeclaring]")
+case object NSCAttributesUnique extends NSError("[NSC: Attributes Unique]")
+
+/** Represents a Well-formedness constraint as defined in the XML specification. */
+sealed abstract class WFError(name: String) extends XmlError(name)
+case object WFCElementTypeMatch extends WFError("[WFC: Element Type Match]")
+case object WFCEntityDeclared extends WFError("[WFC: Entity Declared]")
+case object WFCNoRecursion extends WFError("[WFC: No Recursion]")
---
+++ modules/fs2-data-xml/src/main/scala/fs2/data/xml/package.scala
@@ -0,0 +1,65 @@
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+/*
+ * Copyright 2019 Lucas Satabin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fs2
+package data
+
+import xml.internals._
+
+import cats._
+
+package object xml {
+
+  /** The predefined XML character entities
+    */
+  val xmlEntities = Map("quot" -> "\"", "amp" -> "&", "apos" -> "'", "lt" -> "<", "gt" -> ">")
+
+  /** Transforms a stream of characters into a stream of XML events.
+    * Emitted tokens are guaranteed to be valid up to that point.
+    * If the streams ends without failure, the sequence of tokens is sensured
+    * to represent a (potentially empty) sequence of valid XML documents.
+    */
+  def events[F[_]](implicit F: RaiseThrowable[F]): Pipe[F, Char, XmlEvent] =
+    EventParser.pipe[F]
+
+  /** Resolves the character and entity references in the XML stream.
+    * Entities are already defined and validated (especially no recursion),
+    * hence the `entities` map contains the resolved text to replace the entity by.
+    */
+  def referenceResolver[F[_]](entities: Map[String, String] = xmlEntities)(
+      implicit F: MonadError[F, Throwable]): Pipe[F, XmlEvent, XmlEvent] =
+    ReferenceResolver[F](entities).pipe
+
+  /** Resolves all prefixes in [[QName]]s.
+    * Assumes that entity and character references have been resolved
+    * already. Make stream go through [[referenceResolver]] first if you
+    * need it (not needed if you xml doesn't contain any such reference).
+    */
+  def namespaceResolver[F[_]](implicit F: MonadError[F, Throwable]): Pipe[F, XmlEvent, XmlEvent] =
+    NamespaceResolver[F].pipe
+
+  /** Performs some event normalizations:
+    *  - group consecutive non CDATA [[XmlEvent.XmlString]]s
+    * This can be useful to merge texts once references have been resolved.
+    * Attribute values are also normalized, so that they will end up being
+    * one single string after normalization if references have been replaced.
+    */
+  def normalize[F[_]]: Pipe[F, XmlEvent, XmlEvent] =
+    Normalizer.pipe[F]
+
+}
